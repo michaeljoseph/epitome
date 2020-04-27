96b1547b380e1be833795b99405e81afea9036e2
Michael Joseph <michaeljoseph+github@gmail.com>
2020-04-27 19:56:40 +0200

    Generated project: coloredlogs, colorful, heartrate and emoji

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}.py
@@ -1,16 +1,32 @@
 import logging
+from functools import partial
 
 import click
+import coloredlogs
+import colorful
+import emoji
+import heartrate
+
+log = logging.getLogger(__name__)
+colorful.use_true_colors()
+colorful.use_style('solarized')
+emojize = partial(emoji.emojize, use_aliases=True)
 
 
 @click.option('--debug', help='Enables debug logging.', is_flag=True, default=False)
 @click.group(context_settings=dict(help_option_names=[u'-h', u'--help']))
 def {{ cookiecutter.project_name }}(debug: bool):
     """{{ cookiecutter.description }}"""
-    logging.basicConfig(
-        format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
-        level=logging.DEBUG if debug else logging.INFO,
+    coloredlogs.install(
+        fmt=(
+            '%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s'
+            if debug
+            else '%(asctime)s,%(msecs)d %(levelname)-8s %(message)s'
+        ),
+        level='DEBUG' if debug else 'INFO',
     )
+    if trace:
+        heartrate.trace()
 
 
 @{{ cookiecutter.project_name }}.command()
